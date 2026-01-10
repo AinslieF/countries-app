bee6ae58b4135b597172b2eed53d3e970d75a5e6
AinslieF <ainsliefrancisdec16@gmail.com>
2026-01-09 20:02:25 -0500

    Complete Countries App Version 0

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+/**
+ * Entry point for the React application
+ *
+ * Purpose:
+ * - Mounts the React app to the DOM
+ * - Enables React StrictMode for development checks
+ * - Wraps the app in BrowserRouter to enable routing
+ */
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* 
+      BrowserRouter enables client-side routing
+      so pages can change without reloading the browser
+    */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
